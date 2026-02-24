672e4e60f6f678dd7fd9358123db036b8eb35108
Rony Nedkov <rony.nedkov@ilent.nl>
2026-02-24 08:17:50 +0100

    Add secrets-example. Refactor mqtt connection

---
+++ code/FireBeetle/skyping/secrets-example.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define WIFI_SSID "WIFI NAME"
+#define WIFI_PASSWORD "PASWSWORD"
+#define MQTT_HOST IPAddress(192, 168, 1, 1)
+#define MQTT_PORT 1883
+
+#endif
--- code/FireBeetle/skyping/skyping.ino
+++ code/FireBeetle/skyping/skyping.ino
@@ -1,14 +1,12 @@
-#include <DHT.h>
 #include <WiFi.h>
-#include <Ticker.h>
-#include <AsyncMqtt_Generic.h>
+#include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include "secrets.h"
 #include "CHT8305.h"
 
 #define uS_TO_S_FACTOR 1000000ULL // Conversion factor from microseconds to seconds
-#define TIME_TO_SLEEP  600         // Time (in seconds) for ESP32-E to enter deep sleep
-RTC_DATA_ATTR int bootCount = 0;    
+#define TIME_TO_SLEEP  900        // Time (in seconds) for ESP32-E to enter deep sleep
+RTC_DATA_ATTR int bootCount = 0;
 
 #define TEMT6000 A0
 
@@ -16,25 +14,22 @@ RTC_DATA_ATTR int bootCount = 0;
 #define ACTIVATE_SENSOR_PIN D13
 
 // CHT8305 temperature and humidity sensor
-CHT8305 CHT(0x40);  
+CHT8305 CHT(0x40);
 
-float temp = 0.0;
-float hum = 0.0;
-
-#define MQTT_HOST IPAddress(192, 168, 2, 46)
-#define MQTT_PORT 1883
+const char* mqtt_server = "192.168.178.121";
 
 unsigned long previousMillis = 0;  // Stores last time sensor reading was published
 const long interval = 10000;       // Interval at which to publish sensor readings
 
-AsyncMqttClient mqttClient;
-Ticker mqttReconnectTimer;
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 
 const char* topic_skyping = "esp/skyping";
 
-void print_wakeup_reason(){          
-  esp_sleep_wakeup_cause_t wakeup_reason;  
-  wakeup_reason = esp_sleep_get_wakeup_cause(); 
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+  wakeup_reason = esp_sleep_get_wakeup_cause();
 
   switch(wakeup_reason) {             // Check the wake-up reason and print the corresponding message
     case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
@@ -46,6 +41,16 @@ void print_wakeup_reason(){
   }
 }
 
+//Helper functon for notifications using on board led
+void blinkLed(uint8_t times, uint16_t onMs = 80, uint16_t offMs = 80) {
+    for (uint8_t i = 0; i < times; i++) {
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(onMs);
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(offMs);
+    }
+}
+
 void initWiFi() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -55,106 +60,105 @@ void initWiFi() {
     delay(1000);
   }
   Serial.println(WiFi.localIP());
-  connectToMqtt();
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
-void connectToMqtt() {
-  Serial.println("Connecting to MQTT...");
-  mqttClient.connect();
-}
+void reconnectMQTT() {
+  while (!client.connected()) {
+    Serial.print("Connecting to MQTT...");
 
-void onMqttConnect(bool sessionPresent) {
-  Serial.println("Connected to MQTT.");
-  Serial.print("Session present: ");
-  Serial.println(sessionPresent);
-  Serial.println("===== COLLECTING DATA ===== ");
-
-  CHT.read();
-
-  temp = CHT.getTemperature();
-  hum = CHT.getHumidity();
-
-  float volts = analogRead(TEMT6000) * 3.3 / 1024.0;        // Convert reading to VOLTS
-
-  //Conversions from reading to LUX
-  float amps = volts / 10000.0;      // em 10,000 Ohms
-  float microamps = amps * 1000000;  // Convert to Microamps
-  float lux = microamps * 2.0;       // Convert to Lux */
-  Serial.println("----------------");
-  Serial.print("LUX - ");
-  Serial.print(lux);
-  Serial.println(" lx");
-  Serial.print(volts);
-  Serial.println(" volts");
-  Serial.println("----------------");
-
-  DynamicJsonDocument doc(1024);
-  doc["sensor_id"] = 1;
-  doc["project_id"] = 1;
-  doc["project_name"] = "sky-ping";
-  doc["temperature"] = round(temp * 10.0) / 10.0;
-  doc["humidity"] = round(hum * 10.0) / 10.0;
-  doc["lux"] = round(lux);
-
-
-  String jsonString;
-  serializeJson(doc, jsonString);
-
-  Serial.println("===== SENDING DATA ===== ");
-  uint16_t packetIdPub1 = mqttClient.publish(topic_skyping, 1, true, jsonString.c_str());
-}
-
-void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
-  Serial.println("Disconnected from MQTT.");
-
-  if (WiFi.isConnected()) {
-    mqttReconnectTimer.once(2, connectToMqtt);
+    if (client.connect("ESP32-skyping")) {
+      Serial.println(" connected!");
+    } else {
+      Serial.print(" failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" retrying in 5 seconds");
+      delay(5000);
+    }
   }
-}
-
-void onMqttPublish(uint16_t packetId) {
-  Serial.print("Publish acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-
-  Serial.println("===== POWER OFF SENSORS ===== ");
-  digitalWrite(ACTIVATE_SENSOR_PIN, LOW);
-  Serial.println("===== GOING BACK TO SLEEP ===== ");
-  esp_deep_sleep_start();   
 }
 
 void setup() {
     Serial.begin(115200);
-    delay(1000);
-    ++bootCount;
-    Serial.println("Boot number: " + String(bootCount));   
-    print_wakeup_reason();  
-    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);   
+    delay(500);
 
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(ACTIVATE_SENSOR_PIN, OUTPUT);
+
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("Powering ON sensors");
+
+    digitalWrite(ACTIVATE_SENSOR_PIN, HIGH);
+
+    delay(1000);
+
+    ++bootCount;
+    Serial.println("Boot number: " + String(bootCount));
+    print_wakeup_reason();
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+
+    Serial.println("Setting up I2C for CHT sensor");
     Wire.begin();
     Wire.setClock(100000);
     delay(500);
     CHT.begin();
     delay(500);
 
-    pinMode(ACTIVATE_SENSOR_PIN, OUTPUT);
-
-    mqttClient.onConnect(onMqttConnect);
-    mqttClient.onDisconnect(onMqttDisconnect);
-
-    mqttClient.onPublish(onMqttPublish);
-    mqttClient.setServer(MQTT_HOST, MQTT_PORT);
-
-    digitalWrite(ACTIVATE_SENSOR_PIN, HIGH);
-    
     initWiFi();
 
 
-
-    Serial.flush();
+    client.setServer(mqtt_server, MQTT_PORT);
 
 }
 
 void loop() {
+
+  if (!client.connected()) {
+    reconnectMQTT();
+  }
+  client.loop();
+
+  delay(2000);
+
+  Serial.println("Reading sensors");
+
+  CHT.begin();
+  delay(200);
+  if (!CHT.read()) {
+      Serial.println("Sensor read failed");
+  }
+
+  float  temp = CHT.getTemperature();
+  float  hum = CHT.getHumidity();
+
+  Serial.println("----------------");
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.println(" °C");
+
+  Serial.print("Humidity: ");
+  Serial.print(hum);
+  Serial.println(" %");
+  Serial.println("----------------");
+
+  DynamicJsonDocument doc(256);
+  doc["sensor_id"] = 1;
+  doc["temperature"] = round(temp * 10.0) / 10.0;
+  doc["humidity"] = round(hum * 10.0) / 10.0;
+
+  char payload[256];
+  serializeJson(doc, payload);
+
+  Serial.println("Publishing MQTT message:");
+  Serial.println(payload);
+
+  client.publish(topic_skyping, payload);
+
+  Serial.println("Powering OFF sensors");
+  digitalWrite(ACTIVATE_SENSOR_PIN, LOW);
+
+  Serial.flush();
+
+  esp_deep_sleep_start();
 
 }
